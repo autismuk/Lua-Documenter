573f79c1bc26985ad23469fa0cf43df10e8bd02b
Paul Robson <paul@robsons.org.uk>
2014-05-06 10:30:26 +0100

    First working version

--- luadoc.lua
+++ luadoc.lua
@@ -2,6 +2,7 @@
 ---
 ---				Name : 		luadoc.lua
 ---				Purpose :	Script providing simple inline documentation features for my personal coding style.
+---							so it only parses Classes and Methods, nothing else.
 ---				Created:	6 May 2014
 ---				Author:		Paul Robson (paul@robsons.org.uk)
 ---				License:	MIT
@@ -96,17 +97,18 @@ function SourceProcessor:process(sourceFile)
 			end 
 								
 			local class,method,parameters =														-- detect a function definition <class>:<method>(<parameters>), again class capitalised. 
-									line:match("function%s+([A-Z]%w*)%:(%w+)%(([%w%,]*)%)")		-- this is very specific to my coding style :)
+									line:match("function%s+([A-Z]%w*)%:([%w%_]+)%(%s*([%w%,]*)%s*%)")		-- this is very specific to my coding style :)
 
 			if class ~= nil then 																-- found a method definition.
 				assert(self.classes[class] ~= nil,"Class unknown in method def "..line) 		-- the class must have been defined already.
 				local thisClass = self.classes[class] 											-- keep a reference to it.
+				if method == "initialise" then method = "(constructor)" end 					-- rename initialise.
 				assert(thisClass.methods[method] == nil,"Duplicate method "..line)				-- check the method doesn't already exist.
 				thisClass.methods[method] = self.current 										-- save doc store in methods table for the class.
 				self.current.name = method 														-- name is method - this is kept in the methods table of the class
 				self.current.parameters = parameters 											-- save the parameter list
 				self.current = DocStore:new() 													-- a new document store
-				print("        Found method "..class..":"..method)
+				print("        Found method "..class..":"..method.."("..parameters..")")
 				-- print(">",class,method,parameters,thisClass.methods[method].comment)
 			end
 		end
@@ -115,6 +117,76 @@ function SourceProcessor:process(sourceFile)
 	print("Parsing complete.")
 end
 
+--//	Render the complete scanned documentation as HTML
+--// 	@handle [I/O Handle] handle to render it to.
 
+function SourceProcessor:renderHTML(handle)
+	local css = "<style> body { background:#006; color:white; font-family:Arial,Verdana;} table,tr,td { padding:4px;border:1px solid white; border-collapse:collapse; } </style>"
+	handle:write("<!DOCTYPE html><html><head>"..css.."</head><body>\n")							-- header
+	local classList = self:getSortedKeys(self.classes) 	 										-- get all the names of the classes
+	for _,class in ipairs(classList) do 														-- work through the classes
+		self:renderClass(handle,self.classes[class]) 											-- render the class.
+		local methodlist = self:getSortedKeys(self.classes[class].methods) 						-- get all the names of its methods
+		for _,method in ipairs(methodlist) do 													-- work through them
+			self:renderMethod(handle,self.classes[class].methods[method]) 						-- and render them.
+		end
+	end
+	handle:write("<hr><p><i>LUA Autodoc by Paul Robson 2014</i></p><hr>")
+	handle:write("</body></html>\n") 															-- get sorted list of classes
+end
+
+
+--//	Render documentation for a class in HTML
+--//	@handle 	[i/o handle]		File handle
+--//	@class 		[DocStore]			Documentation for that class
+
+function SourceProcessor:renderClass(handle,class)
+	handle:write("<hr><h1>" .. class.name .. "</h1>\n")
+	handle:write("<p><b>Extends " .. class.baseClass .. "</b></p>\n")
+	handle:write("<p>"..class.comment.."</p><hr>\n")
+end
+
+--//	Render documentation for a method in HTML
+--//	@handle 	[i/o handle]		File handle
+--//	@method 	[DocStore]			Documentation for that method
+
+function SourceProcessor:renderMethod(handle,method)
+	handle:write("<h2>"..method.name.."("..method.parameters..")</h2>\n")
+	if method.parameters ~= "" then 															-- process parameters
+		handle:write("<table>\n")
+		local params = method.parameters .. "," 												-- make sure there's a trailing comma
+		if method.attributes["return"] ~= nil then params = params .. "return," end 			-- if @return was specified add it.
+		while params ~= "" do 																	-- keep going till finished.
+			local p = params:match("^(%w+),") 													-- extract parameter.
+			params = params:sub(#p+2) 															-- remove from string.
+			handle:write("<tr>")
+			handle:write("<td>"..p.."</td>")
+			local typeDesc = "" 																-- type descriptor
+			local paramComment = method.attributes[p:lower()] or "" 							-- get the attribute if there is one.
+			local td,pc = paramComment:match("^%[(%w+)]%s*(.*)$")
+			if td ~= nil then typeDesc = td paramComment = pc end
+
+			handle:write("<td>"..typeDesc.."</td><td>"..paramComment.."</td>")
+			handle:write("</tr>\n")
+		end
+		handle:write("</table>\n")
+	end
+	handle:write("<p>"..method.comment.."</p>\n")
+end
+
+--//	Return a sorted list of the keys in a table
+--//	@inTable [table]	table whose keys are required
+--//	@return 			sorted list of the keys
+
+function SourceProcessor:getSortedKeys(inTable)
+	local keys = {} 																			-- result
+	for key,_ in pairs(inTable) do keys[#keys+1] = key end 										-- copy keys into list
+	table.sort(keys)																			-- sort the table
+	return keys
+end
 local sp = SourceProcessor:new()
 sp:process("luadoc.lua")
+
+h = io.open("doc.html","w")
+sp:renderHTML(h)
+h:close()
